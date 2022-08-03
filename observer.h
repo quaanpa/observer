c999acdd43dae5e1c4ae2f77da1b482893837c71
Phan Anh Quan (MS/EMC21-XC) <pqh3hc@bosch.com>
2022-08-03 18:12:47 +0700

    abc

--- observer.h
+++ observer.h
@@ -1,27 +1,11 @@
 #ifndef OBSERVER_H_INCLUDED
 #define OBSERVER_H_INCLUDED
 
-class iSubcriber
-{
-public:
-    virtual void update;
-};
-class iPublisher
-{
-public:
-    virtual void registerSubcriber;
-    virtual void removeSubcriber;
-    virtual void notify;
-};
 
-class Subcriber : class iSubcriber
-{
 
-};
 
-class Publisher : class iPublisher
-{
 
-};
+
+
 
 #endif // OBSERVER_H_INCLUDED
